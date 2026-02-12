0e65c2d4d336865cde4403c0442bd22dd1f00a2e
charan <charanrajueluri@gmail.com>
2026-02-12 11:18:33 +0530

    core java basics

--- Vcube/src/daily_practice/Temperature.java
+++ Vcube/src/daily_practice/Temperature.java
@@ -5,6 +5,18 @@ public class Temperature {
 		double cel=30;
 		double f= ((cel*9/5)+32);
 		System.out.printf("Fahren heit: %.2f\n",f);
-		System.out.println(cel);
+//		System.out.println(cel);
+		Temperature t=new Temperature();
+		t.test(cel);
+		test1(cel);
+		
+	}
+	void test(double cel) {
+		double f= ((cel*9/5)+32);
+		System.out.printf("Fahren heit: %.2f\n",f);
+	}
+	static void test1(double cel) {
+		double f= ((cel*9/5)+32);
+		System.out.printf("Fahren heit: %.2f\n",f);
 	}
 }
